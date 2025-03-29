204d2fb2aaaa34748ed4b477f218420f73caab25
fine-agent <fine@fine.dev>
2025-03-29 22:57:58 +0000

    Fix build script, add theme provider, update main.tsx imports.

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router";
 import { TooltipProvider } from "./components/ui/tooltip";
 import { ThemeProvider } from "./components/layout/theme-provider";
 import "./index.css";
